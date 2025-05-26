39ab71421ff16fa6976fa145a8373aa381020c72
Ajar Dahal <ajdahal@ucsc.edu>
2025-05-25 21:43:42 -0700

    adding another draft

--- Cube.js
+++ Cube.js
@@ -1,65 +1,81 @@
 class Cube {
-    constructor() {
-      this.type = 'cube';
-      this.color = [1.0, 1.0, 1.0, 1.0];
-      this.matrix = new Matrix4();
-      this.textureNum=-1;
-    }
-  
-    render() {
-      var rgba = this.color;
-  
-      gl.uniform1i(u_whichTexture, this.textureNum);
-      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-      gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
-  
-      drawTriangle3DUV(
-        [0.0, 0.0, 0.0,   1.0, 1.0, 0.0,   1.0, 0.0, 0.0],
-        [0, 0,  1, 1,  1, 0]
-      );
-      drawTriangle3DUV(
-        [0.0, 0.0, 0.0,   0.0, 1.0, 0.0,   1.0, 1.0, 0.0],
-        [0, 0,  0, 1,  1, 1]
-      );
-      drawTriangle3DUV(
-        [0.0, 0.0, 1.0,   1.0, 1.0, 1.0,   1.0, 0.0, 1.0],
-        [1, 0,  0, 1,  0, 0]  
-      );
-      drawTriangle3DUV(
-        [0.0, 0.0, 1.0,   0.0, 1.0, 1.0,   1.0, 1.0, 1.0],
-        [1, 0,  1, 1,  0, 1]  
-      );
-      drawTriangle3DUV(
-        [0.0, 1.0, 0.0,   1.0, 1.0, 0.0,   1.0, 1.0, 1.0],
-        [0, 0,  1, 0,  1, 1]  
-      );
-      drawTriangle3DUV(
-        [0.0, 1.0, 0.0,   0.0, 1.0, 1.0,   1.0, 1.0, 1.0],
-        [0, 0,  0, 1,  1, 1]  
-      );
-      drawTriangle3DUV(
-        [0.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 1.0],
-        [1, 0,  0, 0,  0, 1]  
-      );
-      drawTriangle3DUV(
-        [0.0, 0.0, 0.0,   0.0, 0.0, 1.0,   1.0, 0.0, 1.0],
-        [1, 0,  1, 1,  0, 1]  
-      );
-      drawTriangle3DUV(
-        [1.0, 0.0, 0.0,   1.0, 1.0, 0.0,   1.0, 1.0, 1.0],
-        [0, 0,  0, 1,  1, 1]  
-      );
-      drawTriangle3DUV(
-        [1.0, 0.0, 0.0,   1.0, 0.0, 1.0,   1.0, 1.0, 1.0],
-        [0, 0,  1, 0,  1, 1]  
-      );
-      drawTriangle3DUV(
-        [0.0, 0.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 1.0],
-        [1, 0,  1, 1,  0, 1]  
-      );
-      drawTriangle3DUV(
-        [0.0, 0.0, 0.0,   0.0, 0.0, 1.0,   0.0, 1.0, 1.0],
-        [1, 0,  0, 0,  0, 1]  
-      );
-    }
+  constructor() {
+    this.type = 'cube';
+    this.color = [1.0, 1.0, 1.0, 1.0];
+    this.matrix = new Matrix4();
+    this.textureNum=-1;
   }
+
+  render() {
+    var rgba = this.color;
+
+    gl.uniform1i(u_whichTexture, this.textureNum);
+
+    // Pass the color of a point to u_FragColor variable
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+    // Pass the matrix to u_ModelMatrix attribute
+    gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+    drawTriangle3DUVNormal(
+      [0.0, 0.0, 0.0,   1.0, 1.0, 0.0,   1.0, 0.0, 0.0],
+      [0, 0,  1, 1,  1, 0],
+      [0,0,-1,  0,0,-1,  0,0,-1]
+    );
+    drawTriangle3DUVNormal(
+      [0.0, 0.0, 0.0,   0.0, 1.0, 0.0,   1.0, 1.0, 0.0],
+      [0, 0,  0, 1,  1, 1],
+      [0,0,-1,  0,0,-1,  0,0,-1]
+    );
+    drawTriangle3DUVNormal(
+      [0.0, 0.0, 1.0,   1.0, 1.0, 1.0,   1.0, 0.0, 1.0],
+      [1, 0,  0, 1,  0, 0],
+      [0,0,1,  0,0,1,  0,0,1]
+    );
+    drawTriangle3DUVNormal(
+      [0.0, 0.0, 1.0,   0.0, 1.0, 1.0,   1.0, 1.0, 1.0],
+      [1, 0,  1, 1,  0, 1],
+      [0,0,1,  0,0,1,  0,0,1]
+    );
+    drawTriangle3DUVNormal(
+      [0.0, 1.0, 0.0,   1.0, 1.0, 0.0,   1.0, 1.0, 1.0],
+      [0, 0,  1, 0,  1, 1],
+      [0,1,0,  0,1,0,  0,1,0]
+    );
+    drawTriangle3DUVNormal(
+      [0.0, 1.0, 0.0,   0.0, 1.0, 1.0,   1.0, 1.0, 1.0],
+      [0, 0,  0, 1,  1, 1],
+      [0,1,0,  0,1,0,  0,1,0]
+    );
+    drawTriangle3DUVNormal(
+      [0.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 1.0],
+      [1, 0,  0, 0,  0, 1],
+      [0,-1,0,  0,-1,0,  0,-1,0]
+    );
+    drawTriangle3DUVNormal(
+      [0.0, 0.0, 0.0,   0.0, 0.0, 1.0,   1.0, 0.0, 1.0],
+      [1, 0,  1, 1,  0, 1],
+      [0,-1,0,  0,-1,0,  0,-1,0]
+    );
+    drawTriangle3DUVNormal(
+      [1.0, 0.0, 0.0,   1.0, 1.0, 0.0,   1.0, 1.0, 1.0],
+      [0, 0,  0, 1,  1, 1],
+      [1,0,0,  1,0,0,  1,0,0]
+    );
+    drawTriangle3DUVNormal(
+      [1.0, 0.0, 0.0,   1.0, 0.0, 1.0,   1.0, 1.0, 1.0],
+      [0, 0,  1, 0,  1, 1],
+      [1,0,0,  1,0,0,  1,0,0]
+    );
+    drawTriangle3DUVNormal(
+      [0.0, 0.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 1.0],
+      [1, 0,  1, 1,  0, 1],
+      [-1,0,0,  -1,0,0,  -1,0,0]
+    );
+    drawTriangle3DUVNormal(
+      [0.0, 0.0, 0.0,   0.0, 0.0, 1.0,   0.0, 1.0, 1.0],
+      [1, 0,  0, 0,  0, 1],
+      [-1,0,0,  -1,0,0,  -1,0,0]
+    );
+  }
+}
